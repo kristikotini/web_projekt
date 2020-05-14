044103b02adb91e6239b5cc7bb50ef7151bcf2c2
Megan <megan.ismaili@fshnstudent.info>
2020-05-14 13:40:11 +0200

    krijimi student.php

---
+++ student.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
+        <script src="assets\js\jquery-3.4.1.min.js"></script>
+        <script src="assets\js\popper.min.js"></script>
+        <script src="assets\js\bootstrap.min.js"></script>
+        <script src="assets\fonts\all.min.js"></script>
+        <link rel="stylesheet" href="assets/css/custom.css">
+        <link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.css">
+    </head>
+
+    <body>
+    <?php
+    include 'header.php';
+    ?>
+
+
+
+
+
+
+
+
+   <?php
+    include 'footer.php';
+    ?>
+   </body>
+   </html>
