968ff5527b8fa59ed32f65cd4707924ee626de2e
Megan <megan.ismaili@fshnstudent.info>
2020-05-16 03:27:33 +0200

    funksionaliteti1,2

--- student.php
+++ student.php
@@ -17,9 +17,67 @@
     <body>
     <?php
     include 'header.php';
+    include 'db_connection.php';
     ?>
 
+    <div id="te_dhena">
+        <?php
+            $id=$_SESSION["id"];
 
+            $sql1="SELECT emer,mbiemer
+                  FROM perdorues
+                  WHERE perdorues.perdorues_id=$id";
+            $result1=mysqli_query($conn,$sql1);
+
+            while($row=mysqli_fetch_array($result1,MYSQLI_ASSOC)){
+                echo "<h2>".$row["emer"]." ".$row["mbiemer"]."</h2>";
+            }
+
+            $sql2="SELECT grupi,data_regjistrim,program.emer,program.nivel,viti_std,id_programi_fk
+                  FROM student JOIN program
+                  ON student.id_programi_fk=program.id_programi
+                  WHERE student.id_student_fk=$id";
+            $result2=mysqli_query($conn,$sql2);
+        
+            while($row=mysqli_fetch_array($result2,MYSQLI_ASSOC)){
+                echo "<p> Grupi:".$row["grupi"]." "."Regjistruar me: ".
+                $row["data_regjistrim"]." ".$row["emer"]." ".$row["nivel"]." Ne vitin: ". $row["viti_std"]."</p>";
+                $program=$row["id_programi_fk"];
+            }
+
+        ?>   
+    </div>
+
+
+    <div id="tab_nota">
+        <h3>Struktura akademike</h3>
+        <?php
+            $sql3="SELECT emer,viti_lendes,kredite,kohezgjatja,nota.nota
+                   FROM lenda LEFT JOIN nota
+                   ON nota.id_perdorues_fk=$id AND lenda.id_lende=nota.id_lende_fk
+                   WHERE lenda.id_programi_fk=$program";
+            $result3=mysqli_query($conn,$sql3);
+
+            $lendetarr=array();
+            array_push($lendetarr,array("Emri i lendes","Viti","Kredite","Kohezgjatja","Nota"));
+
+            while($row=mysqli_fetch_array($result3,MYSQLI_BOTH)){
+                 array_push($lendetarr,array($row["emer"],$row["viti_lendes"],$row["kredite"],$row["kohezgjatja"],$row["nota"]));
+            }
+            
+            echo "<table border=1px>";
+              for($i=0;$i<sizeof($lendetarr);$i++){
+                  echo "<tr>";
+                  for($j=0;$j<sizeof($lendetarr[0]);$j++){
+                      echo "<td>";
+                      echo $lendetarr[$i][$j];
+                      echo "</td>";
+                  }
+                  echo "</tr>";
+              }
+            echo "</table>";
+        ?>
+    </div>
 
 
 
